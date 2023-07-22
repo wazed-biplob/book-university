6f3120992781142539b24db374554f2857113f9e
Wazed Biplob <www.biplob@gmail.com>
2023-07-22 22:28:13 +0600

    Review Section UI completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CollegeCard from "../Section/CollegeCard/CollegeCard";
 import ImageGallary from "../Section/ImageGallary/ImageGallary";
 import ResearchPaper from "../Section/ResearchPaper/ResearchPaper";
+import Review from "../Section/Review/Review";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <CollegeCard />
       <ImageGallary />
       <ResearchPaper />
+      <Review />
     </>
   );
 };
