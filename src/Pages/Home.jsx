7374a1edf9c2802a2ec9168c26b93ef023c842b0
Wazed Biplob <www.biplob@gmail.com>
2023-07-22 22:24:36 +0600

    Research Paper Section UI completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CollegeCard from "../Section/CollegeCard/CollegeCard";
 import ImageGallary from "../Section/ImageGallary/ImageGallary";
+import ResearchPaper from "../Section/ResearchPaper/ResearchPaper";
 
 const Home = () => {
   return (
     <>
       <CollegeCard />
       <ImageGallary />
+      <ResearchPaper />
     </>
   );
 };
