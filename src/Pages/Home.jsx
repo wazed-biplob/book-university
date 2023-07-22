0c98afb4747e46a63be18a2210992abdfcb44e29
Wazed Biplob <www.biplob@gmail.com>
2023-07-22 22:20:22 +0600

    Image Gallery UI completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CollegeCard from "../Section/CollegeCard/CollegeCard";
+import ImageGallary from "../Section/ImageGallary/ImageGallary";
 
 const Home = () => {
   return (
     <>
       <CollegeCard />
+      <ImageGallary />
     </>
   );
 };
